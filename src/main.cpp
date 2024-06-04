134df076ded8455e6856589773cfab59881b483a
Cristian Carreras <cristian.carrerascastillo@gmail.com>
2024-06-03 20:30:55 -0400

    added libraries for NTP sync

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 
 #include <Arduino.h>
 #include <Ultrasonic.h>
+#include <ESP8266WiFi.h>
+#include <time.h>
+#include <env.h>
 
 Ultrasonic pingPing(16); // gpio 16 (D0) 
 
@@ -33,6 +36,26 @@ LedStrip green(0, 50); // gpio 0 (D3), 200 intensity [warning: D3 is used for se
 
 void setup() {
   Serial.begin(9600);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+
+  configTime(-4, 0, "ntp.shoa.cl");
+
+  time_t now = time(nullptr);
+  while (now < 24 * 3600) {
+    Serial.println("Waiting for time to be set...");
+    delay(1000);
+    now = time(nullptr);
+  }
+  Serial.println("Time set!");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.println(asctime(&timeinfo));
 }
 
 void lowWater() {
@@ -75,32 +98,52 @@ void warning(){
     green.off();
     delay(100);
   }
+}
 
+void off(){
+  Serial.println("Apagando leds");
+  red.off();
+  yellow.off();
+  green.off();
 }
+
 void loop() {
-  int distance = 0;
-  distance = pingPing.read();
-  Serial.print("Distancia: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-  
-  switch (distance)
-  {
-  case 0 ... 19:
-    warning();
-    break;
-  case 20 ... 59:
-    highWater();
-    break;
-  case 60 ... 119:
-    mediumWater();
-    break;
-  case 120 ... 155:
-    lowWater();
-    break;
-  default:
-    error();
-    break;
-}
-  delay(5000);
+
+  time_t now = time(nullptr);
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.println(asctime(&timeinfo));
+  if (timeinfo.tm_hour >= 8 || timeinfo.tm_hour < 19) {
+    Serial.println("Encendiendo led, Buenos dias");
+
+    int distance = 0;
+    distance = pingPing.read();
+    Serial.print("Distancia: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+    
+    switch (distance)
+    {
+    case 0 ... 19:
+      warning();
+      break;
+    case 20 ... 59:
+      highWater();
+      break;
+    case 60 ... 119:
+      mediumWater();
+      break;
+    case 120 ... 155:
+      lowWater();
+      break;
+    default:
+      error();
+      break;
+  }
+    delay(5000);
+  } 
+  else {
+    off();
+    delay(1000 * 60 * 60 * 1); // 1 hr
+  }
 }
